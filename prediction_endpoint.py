73c76e982e7550434df9afecce11886b7cfcd0ef
abdelkareemkobo <kareem01095134688@gmail.com>
2023-08-31 22:07:47 +0300

    Initial Commit

---
+++ prediction_endpoint.py
@@ -0,0 +1,69 @@
+import contextlib
+import os
+
+import joblib
+from fastapi import Depends, FastAPI, status
+from pydantic import BaseModel
+from sklearn.pipeline import Pipeline
+
+
+class PredictionInput(BaseModel):
+    text: str
+
+
+class PredictionOutput(BaseModel):
+    category: str
+
+
+memory = joblib.Memory(location="cache.joblib")
+
+
+@memory.cache(ignore=["model"])
+def predict(model: Pipeline, text: str) -> int:
+    prediction = model.predict([text])
+    return prediction[0]
+
+
+class NewsgroupsModel:
+    model: Pipeline | None = None
+    targets: list[str] | None = None
+
+    def load_model(self) -> None:
+        """Load the model"""
+        model_file = os.path.join(os.path.dirname(__file__), "newsgroups_model.joblib")
+        loaded_model: tuple[Pipeline, list[str]] = joblib.load(model_file)
+        model, targets = loaded_model
+        self.model = model
+        self.targets = targets
+
+    async def predict(self, input: PredictionInput) -> PredictionOutput:
+        """Runs a prediction"""
+        if not self.model or not self.targets:
+            raise RuntimeError("Model not loaded")
+        prediction = self.model.predict([input.text])
+        category = self.targets[prediction[0]]
+        return PredictionOutput(category=category)
+
+
+newsgroups_model = NewsgroupsModel()
+
+
+@contextlib.asynccontextmanager
+async def lifespan(app: FastAPI):
+    newsgroups_model.load_model()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+@app.post("/prediction")
+async def prediction(
+    output: PredictionOutput = Depends(newsgroups_model.predict),
+) -> PredictionOutput:
+    return output
+
+
+@app.delete("/cache", status_code=status.HTTP_204_NO_CONTENT)
+def delete_cache():
+    memory.clear()
---
+++ readme.md
@@ -0,0 +1,23 @@
+# Text classification with Fastapi and Sklearn with Joblib 
+# Introduction 
+
+# Technologies 
+rember to mention the versions for every tech 
+# Features 
+what this project provide now and in the future 
+
+# Setup 
+
+# Table of contents 
+
+# Illustrations 
+
+# Scope of functionalities 
+
+# Example of use 
+
+# Project status 
+
+# Sources 
+
+# Other information 
